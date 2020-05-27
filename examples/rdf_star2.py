d5b01215eee187f583ac35ec8e8da4601e1d642b
pragya16067 <pragya16067@iiitd.ac.in>
2020-05-27 21:05:57 +0530

    More complex Examples added

--- examples/rdf_star2.py
+++ examples/rdf_star2.py
@@ -2,7 +2,7 @@ from rdflib import Graph
 from rdflib.namespace import RDF, Namespace
 from pyparsing import ParseException
 
-# For graphs with re-ification as the object rather than subject
+# For graphs with multiple rdf reification/* statements
 
 rdf_graph = """
     PREFIX ex:<http://example.org/>
@@ -17,8 +17,15 @@ rdf_graph = """
     rdf:predicate foaf:age ;
     rdf:object 23 .
     
-    ex:PeopleInfo dct:isSourceOf _:s ;
-        ex:hasURL <http://example.com/crawlers#c1> .
+    _:s2 rdf:type rdf:Statement ;
+    rdf:subject ex:bob ;
+    rdf:predicate foaf:name ;
+    rdf:object "Bob" .
+    
+    _:s2 foaf:relatedTo _:s .
+
+    _:s dct:creator <http://example.com/crawlers#c1> ;
+        dct:source <http://example.net/listing.html> .
 
     ex:welles foaf:name "John Welles" ;
                 ex:mentioned ex:kubrick .
@@ -40,21 +47,19 @@ sparql_query = """
     PREFIX foaf:<http://xmlns.com/foaf/0.1/>
     PREFIX dct:<http://purl.org/dc/terms/>
 
-    SELECT ?x ?src WHERE {
-    {?x foaf:age ?age .
+    SELECT ?x ?y ?name ?age WHERE {
+    ?x foaf:age ?age .
     ?r rdf:type rdf:Statement ;
     rdf:subject ?x ;
     rdf:predicate foaf:age ;
-    rdf:object ?age .
-    ex:PeopleInfo dct:isSourceOf ?r ;
-    ex:hasURL ?src .}
-    UNION {
-    ?x ex:influencedBy ?y .
+    rdf:object ?age ;
+    dct:source ?src . 
+    
+    ?y foaf:name ?name .
     ?r2 rdf:type rdf:Statement ;
     rdf:subject ?x ;
-    rdf:predicate ex:influencedBy ;
-    rdf:object ?y ;
-    dct:source ?src .}
+    rdf:predicate foaf:name ;
+    rdf:object ?name .
     }
 """
 
@@ -64,9 +69,9 @@ rdf_Star_graph = """
     PREFIX foaf:<http://xmlns.com/foaf/0.1/>
     PREFIX dct:<http://purl.org/dc/terms/>
 
-    ex:bob foaf:name "Bob" .
-    ex:PeopleInfo dct:isSourceOf <<ex:bob foaf:age 23>> ;
-        ex:hasURL <http://example.com/crawlers#c1> .
+    <<ex:bob foaf:age 23>> foaf:relatedTo <<ex:bob foaf:name "Bob">> ;
+    dct:creator <http://example.com/crawlers#c1> ;
+    dct:source <http://example.net/listing.html> .
 
     ex:welles foaf:name "John Welles" ;
                 ex:mentioned ex:kubrick .
@@ -82,13 +87,8 @@ sparql_star_query = """
     PREFIX foaf:<http://xmlns.com/foaf/0.1/>
     PREFIX dct:<http://purl.org/dc/terms/>
 
-    SELECT ?x ?age ?src WHERE {
-    {
-    ex:PeopleInfo dct:isSourceOf <<?x foaf:age ?age>> ;
-    ex:hasURL ?src . }
-    UNION
-    { <<?x ex:influencedBy ?y>> dct:source ?src . }
-    }
+    SELECT ?x ?y ?name ?age WHERE 
+    { <<?x foaf:age ?age>> foaf:relatedTo <<?y foaf:name ?name>> .}
 
 """
 
@@ -105,10 +105,10 @@ def test_rdf_star():
     g = Graph()
     g.parse(data=rdf_Star_graph, format="turtle")
 
-    for row in g.query(sparql_star_query):
+    for row in g.query(sparql_query):
         print(row)
 
 
 if __name__ == '__main__':
-    #test_rdf_basic()
+    test_rdf_basic()
     test_rdf_star()
